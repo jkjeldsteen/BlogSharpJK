b8e266322d0ca7e4b42e5d2d971205fe2d45fa93
JakobUCN <jkjeldsteen@gmail.com>
2022-11-16 09:56:12 +0100

    Updates to Controllers

---
+++ BlogSharpJK.MVC/Models/BlogPost.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogSharpJK.MVC.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "Untitled";
+        public string Content { get; set; } = "No content";
+        public DateTime CreationDate { get; set; }
+    }
+}

0ff3294bc77d7b1aefee40180456fbb94c7ef8a0
JakobUCN <93514767+JakobUCN@users.noreply.github.com>
2022-11-16 09:17:21 +0100

    Add project files.

---
+++ BlogSharpJK.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogSharpJK.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
